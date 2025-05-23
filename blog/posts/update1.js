536d2bc2327f219056128425c55a3eb249631554
nonagon <77363641+nonagonn@users.noreply.github.com>
2025-05-23 05:52:23 -0700

    Update update1.js

--- blog/posts/update1.js
+++ blog/posts/update1.js
@@ -2,15 +2,77 @@ window.blogPosts = window.blogPosts || [];
 window.blogPosts.push({
     title: "Speedin' Shotgun Progress Update #1",
     date: "2025-05-27",
-    content: `## Anniversry
+    content: `## Anniversary
 
-here is my audio
+Speedin' Shotgun is 2 years old. Hooray!
 
-[AUDIO:apple3.wav]
+To celebrate this, we here at In New Windows (3 people) decided to post our first blog post, regarding the status of the game! Hooray?
 
-## John IOmage
+So.
 
-Here is my image
+## The Important News
 
-[IMG:darkphillip.png]`
+Well..... Speedin' Shotgun is being *ENTIRELY* remade from the ground up.
+
+[IMG:update1angry.png]
+
+Wait wait wait! Before you give me a columbian necktie and feed me to the buzzards, hear me out for a second.
+
+## Reasons
+
+There are MANY. I'll go over them!
+
+The original project file is TWO YEARS OLD. ( I have progressed as developer significantly since then. )
+
+The current codebase isn't modular at all and very hard to work with.
+
+Implementing controller support in this project file is tedious. Very tedious.
+
+A lot of assets were outdated, and needed a rehaul anyways.
+
+The Level Editor's main script is about 1.5k lines long. I cannot deal with this shit anymore.
+
+
+Now, with the NEW project file;
+
+Fresh start. I am a MUCH better developer now.
+
+The new codebase can be developed to be modular and easy to work with. ( Significant progress on this has already been made. )
+
+Controller support has already been implemented. See for yourself!
+
+[IMG:controllersupport.gif]
+..Ignore the visuals. Temporary.
+
+Assets can be rehauled and replaced easily.
+
+The Level Editor's main script is about 0 lines long. I haven't started working on it yet. ( Though, the Level Editor will be significantly more modular and easy for me to add new objects, data, and more. I've written code for this already, so I lied about it being 0 lines long. It works great. )
+
+[IMG:update1ah.png]
+
+See!! Don't kill me!!!
+
+## When
+
+Well.... We don't know. Maybe 1 year? Maybe 2 years? Maybe 527 years? If I knew, then I'd tell you.
+
+But, we have goals!
+
+The DEMO is intended to release by the end of the year.
+
+The FULL GAME is intended to release whenever it's done. Fuck you.
+
+## That's it??? Nothing new???
+
+Fine. I'll give you a song.
+
+[AUDIO:RicoSHIT.ogg]
+
+New main theme...
+
+I would share more, but we're going through an overhaul right now. Come back later. And I don't want to spoil anything; there's many songs and characters!!! More than you might think!!! But you'll have to wait for the game to hear and see that!!!
+
+## End
+
+2 years huh? I can't wait to be done with this shit.`
 }); 
